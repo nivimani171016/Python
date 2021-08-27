822afb729613413f547387916b2904a741371a17
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-08-27 13:11:40 +0530

    Create last_ele.py

---
+++ last_ele.py
@@ -0,0 +1,4 @@
+#Assign the last element of lst to the variable end_elem. Do this so that it works no matter how long lst is.
+lst = ["hi", "goodbye", "python", "106", "506", 91, ['all', 'Paul', 'Jackie', "UMSI", 1, "Stephen", 4.5], 109, "chair", "pizza", "wolverine", 2017, 3.92, 1817, "account", "readings", "papers", 12, "facebook", "twitter", 193.2, "snapchat", "leaders and the best", "social", "1986", 9, 29, "holiday", ["women", "olympics", "gold", "rio", 21, "2016", "men"], "26trombones"]
+end_elem = lst[-1]
+print(end_elem)
