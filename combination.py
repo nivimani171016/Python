44e313eeb9435dbc7d09b25504df1d925ddec8cb
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-08-27 13:07:46 +0530

    Create combination.py

---
+++ combination.py
@@ -0,0 +1,7 @@
+#Write code that combines the following variables so that the sentence “You are doing a great job, keep it up!” is assigned to the variable message. Do not edit the values assigned to by, az, io, or qy.
+by = "You are"
+az = "doing a great "
+io = "job"
+qy = "keep it up!"
+message = by +" "+ az +io + ", "+qy
+print(message)
