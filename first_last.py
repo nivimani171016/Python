3a624a4cae072a9ab9d1fc7a8c1e5ccaf040f4f4
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-08-27 13:07:05 +0530

    Create first_last.py

---
+++ first_last.py
@@ -0,0 +1,4 @@
+#Write a program that extracts the last three items in the list sports and assigns it to the variable last. Make sure to write your code so that it works no matter how many items are in the list.
+sports = ['cricket', 'football', 'volleyball', 'baseball', 'softball', 'track and field', 'curling', 'ping pong', 'hockey']
+last = sports[-3:]
+print(last)
