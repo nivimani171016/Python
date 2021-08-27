cd9fa55f2f0ec3852b4d57b8c8ed2a15abb4f0d8
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-08-27 13:08:38 +0530

    Create count.py

---
+++ count.py
@@ -0,0 +1,4 @@
+#Write code to determine how many 9’s are in the list nums and assign that value to the variable how_many. Do not use a for loop to do this.
+nums = [4, 2, 23.4, 9, 545, 9, 1, 234.001, 5, 49, 8, 9 , 34, 52, 1, -2, 9.1, 4]
+how_many = nums.count(9)
+print(how_many)
