8a98ab73952845e031886f8683ed55f49181ea39
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-08-27 14:41:09 +0530

    Create if.py

---
+++ if.py
@@ -0,0 +1,10 @@
+#Create a variable, b, and assign it the value of 15. Then, write code to see if the value b is greater than that of a. If it is, a’s value should be multiplied by 2. If the value of b is less than or equal to a, nothing should happen. Finally, create variable c and assign it the value of the sum of a and b.
+a = 20
+b = 15
+if b>a:
+    a = a*2
+else:
+    a = 20
+    b = 15
+c = a + b
+print(c)
